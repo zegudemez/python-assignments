a191f8e82f3c7bf441602d6bcdaa401098c33927
zegudemez <78298746+zegudemez@users.noreply.github.com>
2021-01-31 12:43:24 +0300

    Update 02 nature of a number.py

--- 02 nature of a number.py
+++ 02 nature of a number.py
@@ -3,10 +3,10 @@ while True:
     try:
         num = input('Enter a number to check its nature : ')
         num = float(num)
-        if num > 1:
+        if num > 0:
             print('positive')
-        elif num < 1:
-            print('negetive')
+        elif num < 0:
+            print('negative')
         elif num == 0:
             print('zero')
         break
