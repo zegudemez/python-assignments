7a2b40e24e331c6d00c255f078b8e856a709074e
zegudemez <78298746+zegudemez@users.noreply.github.com>
2021-01-31 13:03:42 +0300

    Update 03 divisibility.py

--- 03 divisibility.py
+++ 03 divisibility.py
@@ -1,14 +1,17 @@
 #------------3 Program to check whether a number is completely divisible by another number ----------
 while True:
     try:
+        num2 = input('Enter dividend : ')
         num1 = input('Enter divisor : ')
-        num2 = input('Enter divident : ')
         num1 = int(num1)
         num2 = int(num2)
-        if num1 % num2 == 0:
-            print('The two numbers are completely divisble by eachother.')
+        if num2 % num1 == 0:
+            print('The two numbers are completely divisible by eachother.')
         else:
-            print('The two numbers are not completely divisble by eachother.')
+            print('The two numbers are not completely divisible by eachother.')
+            remainder = num2 % num1
+            remainder = str(remainder)
+            print('You should subtract ' + remainder + ' this amount from dividend.')
         break
     except:
         print('Invalid entry. Enter integer values.')
